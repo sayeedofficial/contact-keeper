834410ef18767189c2fa6b4beba82d4890cab034
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-03-24 12:30:04 +0530

    Created Basic Routes

---
+++ Routes/contacts.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send("Get all contacts");
+});
+
+router.post("/", (req, res) => {
+  res.send("Add contacts");
+});
+
+router.put("/:id", (req, res) => {
+  res.send("Update contacts");
+});
+
+router.delete("/:id", (req, res) => {
+  res.send("Delete contacts");
+});
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to Contact Keeper API" });
+});
+
+app.use("/api/users", require("./Routes/users"));
+app.use("/api/auth", require("./Routes/auth"));
+app.use("/api/contacts", require("./Routes/contacts"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server started on PORT ${port}`);
+});
