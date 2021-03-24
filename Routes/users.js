ace89b4275712a02d974533553bde024f9d740a5
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-03-24 16:47:39 +0530

    Added the funtionality of saving all the incoming data to database with hashing password

--- Routes/users.js
+++ Routes/users.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator");
+const {
+  check,
+  validationResult,
+  sanitizeCookie,
+} = require("express-validator");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 router.post(
   "/",
@@ -15,12 +20,32 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("Passed");
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "User already exsits" });
+      }
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send("User saved");
+    } catch (err) {
+      console.log(error.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
