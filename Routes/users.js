136ad3e927b5acf6046aed60e85c1511cd70a2c7
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-03-24 16:37:33 +0530

    Added a validation for the data coming from the body

--- Routes/users.js
+++ Routes/users.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
+const User = require("../models/User");
 
-router.post("/", (req, res) => {
-  res.send("Registers a users");
-});
+router.post(
+  "/",
+  [
+    check("name", "name is required").not().isEmpty(),
+    check("email", "Please include a valid email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send("Passed");
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const connectDB = require( "./config/db" );
+const connectDB = require("./config/db");
 
 connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to Contact Keeper API" });
