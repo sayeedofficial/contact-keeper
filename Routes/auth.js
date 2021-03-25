09688d42738163759bc7bc48cbad391958c765ff
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-03-25 15:30:11 +0530

    Implemented Login Authentication Using JWT

--- Routes/auth.js
+++ Routes/auth.js
@@ -1,12 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 router.get("/", (req, res) => {
   res.send("Get logged in user");
 });
 
-router.post("/", (req, res) => {
-  res.send("Login a user");
-});
+router.post(
+  "/",
+  [
+    check("email", "Please inlcude a valid email").isEmail(),
+    check("password", "Password is required").exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: "Invalid Creditionals" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: "Invalid Creditionals" });
+      }
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000,
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send("Server Error");
+    }
+  }
+);
 
 module.exports = router;
