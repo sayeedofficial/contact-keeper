075d43aa01e32e644b0e9a538b8f7ce48ea3a957
sayeedofficial <56059227+sayeedofficial@users.noreply.github.com>
2021-03-31 19:37:12 +0530

    Implemented Client Side Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,24 @@
-import './App.css';
-function App() {
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import "./App.css";
+import Home from "./components/Pages/Home";
+import About from "./components/Pages/About";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <Fragment className="App">
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/Pages/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this app</h1>
+      <p className="my-1">
+        This is Full Stack Application To Store Your Contacts
+      </p>
+      <p className="bg-dark p">
+        <strong> Version :</strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
